76d232724ba55c9f060ef1c8f6d5ecf38b7f62e7
P3DRO1411 <102195393+P3DRO1411@users.noreply.github.com>
2022-04-07 22:19:12 -0300

    Foi add animação de morte colisão com os cactos, estados de jogo. game over e restart

--- sketch.js
+++ sketch.js
@@ -14,7 +14,18 @@ var cactos_image3;
 var cactos_image4;
 var cactos_image5;
 var cactos_image6;
-
+var grupo_nuvens;
+var grupo_cactos;
+var INICIO=1;
+var JOGANDO=2;
+var FIM=3;
+var estados_de_jogo=INICIO;
+var trex_parado;
+var trex_morreu;
+var game_over;
+var reset;
+var game_over_image;
+var reset_image;
 
 
 function preload(){
@@ -27,37 +38,36 @@ function preload(){
   cactos_image4=loadImage("obstacle4.png");
   cactos_image5=loadImage("obstacle5.png");
   cactos_image6=loadImage("obstacle6.png");
+  trex_parado=loadAnimation("trex1.png");
+  trex_morreu=loadAnimation("trex_collided.png");
+  game_over_image=loadImage("gameOver.png");
+  reset_image=loadImage("restart.png");
 }
 
 function pular(){
   if(keyDown("space") && trex.y>=161){
-    trex.velocityY=-7;
+    trex.velocityY=-8;
 
   } else{
     trex.velocityY=trex.velocityY+0.5;
 
   }
-  //console.log(trex.y);
-
-
-
 }
+
 function chao_se_mexe(){
-  ground.velocityX=- 4;
+  ground.velocityX=- 5;
   if(ground.x<0){
     ground.x=ground.width/2;  
 
-
-
   }
-
 
 } 
 
 function criar_cactos(){
   if(frameCount%100===0){
     cactos=createSprite(620,173,10,10);
-    cactos.velocityX=-4;
+    cactos.velocityX=-5;
+    
     cactos.depth=1;
     cactos.lifetime=170;
     cactos.scale=0.70;
@@ -81,12 +91,9 @@ function criar_cactos(){
         cactos.addImage(cactos_image6);     
         break; 
     }
+    grupo_cactos.add(cactos);
   }
-  
 }
-
-
-
 
 function criar_nuvens(){
   if(frameCount%200===0){
@@ -95,11 +102,10 @@ function criar_nuvens(){
     cloud.velocityX=-2;
     cloud.depth=1;
     cloud.lifetime=350;
+    grupo_nuvens.add(cloud);
   }
 
 }
-
-
 
 
 function ver_o_mouse(){
@@ -112,30 +118,73 @@ function ver_o_mouse(){
 function setup(){
   createCanvas(600,200);
   trex=createSprite(30,170,30,30);
+  trex.addAnimation("parado",trex_parado);
   trex.addAnimation("correndo",trex_animation);
+  trex.addAnimation("morto",trex_morreu);
   trex.scale=0.5;
   trex.depth=2;
   ground=createSprite(300,180,600,20);
   ground.addImage("chao",ground_image);
   ground_invisivel=createSprite(300,195,600,20);
   ground_invisivel.visible=false;
-  
+  grupo_cactos=new Group();
+  grupo_nuvens=new Group();
+  game_over=createSprite(312,46,10,10);
+  reset=createSprite(288,120,10,10);
+  game_over.addImage("perdeu", game_over_image);
+  reset.addImage("reset",reset_image);
+  game_over.visible=false;
+  reset.visible=false;
 
 }
 
+function inicio(){
+  if(keyDown("space")){
+    estados_de_jogo=JOGANDO;
+    pular();
+  }
+}
 
-
-
-function draw(){ 
-  background("white");
-  drawSprites();  
-  pular();
-  trex.collide(ground_invisivel);
-  chao_se_mexe();
-  ver_o_mouse();
+function jogando(){
   criar_nuvens();
   numero_aleatorio=Math.round(random(20,80));
   numero_aleatorio2=Math.round(random(1,6));
   criar_cactos();
+  pular();
+  trex.collide(ground_invisivel);
+  chao_se_mexe();
+  trex.changeAnimation("correndo",trex_animation);
+  if(trex.isTouching(grupo_cactos)){
+    estados_de_jogo=FIM;
+  }
 
+
+}
+
+function fim(){
+  ground.velocityX=0;
+  grupo_nuvens.setVelocityXEach(0);
+  grupo_cactos.setVelocityXEach(0);
+  trex.velocityY=0;
+  trex.changeAnimation("morto",trex_morreu);
+  game_over.visible=true;
+  reset.visible=true;
+}
+
+function draw(){ 
+  background("white");
+  drawSprites();  
+  ver_o_mouse();
+  if(estados_de_jogo===FIM){
+    fim();
+
+
+  }
+  
+  if(estados_de_jogo===INICIO){
+    inicio();
+  }
+  if(estados_de_jogo===JOGANDO){
+    jogando();
+  }
 }
